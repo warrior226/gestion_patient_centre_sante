8f4c03b2070ebfc6fa347925784aaa7b901be772
Pato <75866080+warrior226@users.noreply.github.com>
2025-09-09 15:21:27 +0000

    Initialisation du microservice constante-service

---
+++ constante-service/src/main/java/com/wendyam_rayaisse/constanteservice/ConstanteServiceApplication.java
@@ -0,0 +1,13 @@
+package com.wendyam_rayaisse.constanteservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConstanteServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConstanteServiceApplication.class, args);
+    }
+
+}
---
+++ constante-service/src/test/java/com/wendyam_rayaisse/constanteservice/ConstanteServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wendyam_rayaisse.constanteservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConstanteServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
