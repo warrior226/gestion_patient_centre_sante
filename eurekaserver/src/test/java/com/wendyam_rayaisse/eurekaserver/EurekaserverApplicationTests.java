10c59ee4cd3f8fff8561c329cb195c1b8c42f676
Pato <75866080+warrior226@users.noreply.github.com>
2025-09-17 01:27:41 +0000

    eureka server setting

---
+++ eurekaserver/src/test/java/com/wendyam_rayaisse/eurekaserver/EurekaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wendyam_rayaisse.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
