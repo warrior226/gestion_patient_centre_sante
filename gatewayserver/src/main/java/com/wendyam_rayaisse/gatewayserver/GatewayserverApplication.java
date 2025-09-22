7a66f97406d675083ddd895524461b5c60abe44c
prayaisse <prayaisse@ticanalyse.org>
2025-09-22 11:21:30 +0000

    implementing Custom Routing using Spring Cloud Gateway

--- gatewayserver/src/main/java/com/wendyam_rayaisse/gatewayserver/GatewayserverApplication.java
+++ gatewayserver/src/main/java/com/wendyam_rayaisse/gatewayserver/GatewayserverApplication.java
@@ -2,12 +2,46 @@ package com.wendyam_rayaisse.gatewayserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class GatewayserverApplication {
+public class  GatewayserverApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayserverApplication.class, args);
 	}
+
+    @Bean
+    public RouteLocator gestionPatientRouteConfig(RouteLocatorBuilder routeLocatorBuilder){
+
+        return routeLocatorBuilder.routes()
+                .route(p->p
+                        .path("/healthcareApp/medecin-service/**")
+                        .filters(f->f.rewritePath("/healthcareApp/medecin-service/(?<segment>.*)","/${segment}"))
+                        .uri("lb://MEDECIN-SERVICE"))
+                .route(p->p
+                        .path("/healthcareApp/patient-service/**")
+                        .filters(f->f.rewritePath("/healthcareApp/patient-service/(?<segment>.*)","/${segment}"))
+                        .uri("lb://PATIENT-SERVICE"))
+                .route(p->p
+                        .path("/healthcareApp/patient-medecin-service/**")
+                        .filters(f->f.rewritePath("/healthcareApp/patient-medecin-service/(?<segment>.*)","/${segment}"))
+                        .uri("lb://PATIENT-MEDECIN-SERVICE"))
+                .route(p->p
+                        .path("/healthcareApp/disponibilite-service/**")
+                        .filters(f->f.rewritePath("/healthcareApp/disponibilite-service/(?<segment>.*)","/${segment}"))
+                        .uri("lb://DISPONIBILITE-SERVICE"))
+                .route(p->p
+                        .path("/healthcareApp/rdv-service/**")
+                        .filters(f->f.rewritePath("/healthcareApp/rdv-service/(?<segment>.*)","/${segment}"))
+                        .uri("lb://RDV-SERVICE"))
+                .route(p->p
+                        .path("/healthcareApp/consultation-service/**")
+                        .filters(f->f.rewritePath("/healthcareApp/consultation-service/(?<segment>.*)","/${segment}"))
+                        .uri("lb://CONSULTATION-SERVICE")).build();
+
+    }
 
 }
