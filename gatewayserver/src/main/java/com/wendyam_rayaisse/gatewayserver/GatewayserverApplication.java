2c93c3c1a4aa2e3503b1064ca8ac54a6f9f06ea3
prayaisse <prayaisse@ticanalyse.org>
2025-09-22 13:06:33 +0000

    Demo of addResponseHeader filter

--- gatewayserver/src/main/java/com/wendyam_rayaisse/gatewayserver/GatewayserverApplication.java
+++ gatewayserver/src/main/java/com/wendyam_rayaisse/gatewayserver/GatewayserverApplication.java
@@ -6,6 +6,8 @@ import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 
+import java.time.LocalDateTime;
+
 @SpringBootApplication
 public class  GatewayserverApplication {
 
@@ -19,27 +21,33 @@ public class  GatewayserverApplication {
         return routeLocatorBuilder.routes()
                 .route(p->p
                         .path("/healthcareApp/medecin-service/**")
-                        .filters(f->f.rewritePath("/healthcareApp/medecin-service/(?<segment>.*)","/${segment}"))
+                        .filters(f->f.rewritePath("/healthcareApp/medecin-service/(?<segment>.*)","/${segment}")
+                                .addRequestHeader("X-Response-Time", LocalDateTime.now().toString()))
                         .uri("lb://MEDECIN-SERVICE"))
                 .route(p->p
                         .path("/healthcareApp/patient-service/**")
-                        .filters(f->f.rewritePath("/healthcareApp/patient-service/(?<segment>.*)","/${segment}"))
+                        .filters(f->f.rewritePath("/healthcareApp/patient-service/(?<segment>.*)","/${segment}")
+                                .addRequestHeader("X-Response-Time", LocalDateTime.now().toString()))
                         .uri("lb://PATIENT-SERVICE"))
                 .route(p->p
                         .path("/healthcareApp/patient-medecin-service/**")
-                        .filters(f->f.rewritePath("/healthcareApp/patient-medecin-service/(?<segment>.*)","/${segment}"))
+                        .filters(f->f.rewritePath("/healthcareApp/patient-medecin-service/(?<segment>.*)","/${segment}")
+                                .addRequestHeader("X-Response-Time", LocalDateTime.now().toString()))
                         .uri("lb://PATIENT-MEDECIN-SERVICE"))
                 .route(p->p
                         .path("/healthcareApp/disponibilite-service/**")
-                        .filters(f->f.rewritePath("/healthcareApp/disponibilite-service/(?<segment>.*)","/${segment}"))
+                        .filters(f->f.rewritePath("/healthcareApp/disponibilite-service/(?<segment>.*)","/${segment}")
+                                .addRequestHeader("X-Response-Time", LocalDateTime.now().toString()))
                         .uri("lb://DISPONIBILITE-SERVICE"))
                 .route(p->p
                         .path("/healthcareApp/rdv-service/**")
-                        .filters(f->f.rewritePath("/healthcareApp/rdv-service/(?<segment>.*)","/${segment}"))
+                        .filters(f->f.rewritePath("/healthcareApp/rdv-service/(?<segment>.*)","/${segment}")
+                                .addRequestHeader("X-Response-Time", LocalDateTime.now().toString()))
                         .uri("lb://RDV-SERVICE"))
                 .route(p->p
                         .path("/healthcareApp/consultation-service/**")
-                        .filters(f->f.rewritePath("/healthcareApp/consultation-service/(?<segment>.*)","/${segment}"))
+                        .filters(f->f.rewritePath("/healthcareApp/consultation-service/(?<segment>.*)","/${segment}")
+                                .addRequestHeader("X-Response-Time", LocalDateTime.now().toString()))
                         .uri("lb://CONSULTATION-SERVICE")).build();
 
     }
