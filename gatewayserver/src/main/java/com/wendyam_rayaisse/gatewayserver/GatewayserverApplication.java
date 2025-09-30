0d3a9409c4943968fd97d308701e5ee92a50234d
Pato <75866080+warrior226@users.noreply.github.com>
2025-09-30 01:51:02 +0000

    enhancing gateway circuit breaker

--- gatewayserver/src/main/java/com/wendyam_rayaisse/gatewayserver/GatewayserverApplication.java
+++ gatewayserver/src/main/java/com/wendyam_rayaisse/gatewayserver/GatewayserverApplication.java
@@ -1,7 +1,12 @@
 package com.wendyam_rayaisse.gatewayserver;
 
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
+import io.github.resilience4j.timelimiter.TimeLimiterConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.circuitbreaker.resilience4j.ReactiveResilience4JCircuitBreakerFactory;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
+import org.springframework.cloud.client.circuitbreaker.Customizer;
 import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
 import org.springframework.cloud.gateway.filter.ratelimit.RedisRateLimiter;
 import org.springframework.cloud.gateway.route.RouteLocator;
@@ -72,6 +77,12 @@ public class  GatewayserverApplication {
 
     }
 
+    @Bean
+    public Customizer<ReactiveResilience4JCircuitBreakerFactory> defaultCustomizer() {
+        return factory -> factory.configureDefault(id -> new Resilience4JConfigBuilder(id)
+                .circuitBreakerConfig(CircuitBreakerConfig.ofDefaults())
+                .timeLimiterConfig(TimeLimiterConfig.custom().timeoutDuration(Duration.ofSeconds(30)).build()).build());
+    }
 
     @Bean
     public RedisRateLimiter redisRateLimiter(){
