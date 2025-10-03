654cbefc5c33ec0f256a78a79fbbde1a6b82ae83
Pato <75866080+warrior226@users.noreply.github.com>
2025-10-03 01:07:58 +0000

    Develop message microservice using Spring Cloud Function -Part1

---
+++ message/src/main/java/com/wendyam_rayaisse/message/dto/ConsultatiionMsgDto.java
@@ -0,0 +1,4 @@
+package com.wendyam_rayaisse.message.dto;
+
+public record ConsultatiionMsgDto(String patientName,String patientMobileNumber,String patientEmail) {
+}
---
+++ message/src/main/java/com/wendyam_rayaisse/message/functions/MessageFunctions.java
@@ -0,0 +1,8 @@
+package com.wendyam_rayaisse.message.functions;
+
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MessageFunctions {
+}
