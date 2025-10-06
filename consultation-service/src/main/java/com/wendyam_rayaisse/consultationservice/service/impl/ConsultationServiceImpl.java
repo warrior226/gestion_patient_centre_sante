ba6b97f0af168c5ec13ce909ce363c881b505ac3
Pato <75866080+warrior226@users.noreply.github.com>
2025-10-06 00:24:06 +0000

    update message , patient-medecin-service and consultation-service microservices to stream and process the events -Part1

--- consultation-service/src/main/java/com/wendyam_rayaisse/consultationservice/service/impl/ConsultationServiceImpl.java
+++ consultation-service/src/main/java/com/wendyam_rayaisse/consultationservice/service/impl/ConsultationServiceImpl.java
@@ -1,12 +1,16 @@
 package com.wendyam_rayaisse.consultationservice.service.impl;
 
 import com.wendyam_rayaisse.consultationservice.dto.ConsultationDto;
+import com.wendyam_rayaisse.consultationservice.dto.ConsultationMsgDto;
 import com.wendyam_rayaisse.consultationservice.entity.Consultation;
 import com.wendyam_rayaisse.consultationservice.exception.ResourceNotFoundException;
 import com.wendyam_rayaisse.consultationservice.mapper.ConsultationMapper;
 import com.wendyam_rayaisse.consultationservice.repository.ConsultationRepository;
 import com.wendyam_rayaisse.consultationservice.service.IConsultationService;
 import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.stream.function.StreamBridge;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -16,10 +20,25 @@ import java.util.List;
 @AllArgsConstructor
 public class ConsultationServiceImpl implements IConsultationService {
     private ConsultationRepository consultationRepository;
+    private static final Logger log = LoggerFactory.getLogger(ConsultationServiceImpl.class);
+    private final StreamBridge streamBridge;
     @Override
     public void createConsultation(ConsultationDto consultationDto) {
         Consultation consultation = ConsultationMapper.mapToConsultation(consultationDto,new Consultation());
         consultationRepository.save(consultation);
+        sendConsultation(consultation);
+    }
+
+    private void sendConsultation(Consultation consultation){
+        var consultationMsgDto = new ConsultationMsgDto(
+                1L,
+                "RAYAISSE Patrick",
+                "+226 65272174",
+                "rayaissewendyam@gmail.com"
+        );
+        log.info("Sending consultation request for the details: {}", consultationMsgDto);
+        var result = streamBridge.send("sendCommunication-out-0",consultationMsgDto);
+        log.info("Result: {}", result);
     }
 
     @Override
---
+++ patient_medecin-service/src/main/java/com/wendyam_rayaisse/patient_medecinservice/dto/UserMsgDto.java
@@ -0,0 +1,4 @@
+package com.wendyam_rayaisse.patient_medecinservice.dto;
+
+public record UserMsgDto(Long consultationId, String patientName, String patientMobileNumber, String patientEmail) {
+}
