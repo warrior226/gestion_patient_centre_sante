44ff3f6923d3000abbc8f0b9daba911a08e40c8d
prayaisse <prayaisse@ticanalyse.org>
2025-10-06 20:18:58 +0000

    commit du 061025

--- consultation-service/src/main/java/com/wendyam_rayaisse/consultationservice/functions/ConsultationFunctions.java
+++ consultation-service/src/main/java/com/wendyam_rayaisse/consultationservice/functions/ConsultationFunctions.java
@@ -1,4 +1,21 @@
 package com.wendyam_rayaisse.consultationservice.functions;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.function.Consumer;
+
+
+@Configuration
 public class ConsultationFunctions {
+    private static final Logger log = LoggerFactory.getLogger(ConsultationFunctions.class);
+
+    public Consumer<Long> updateCommunication(){
+
+        return patientId->{
+            log.info("the patientId is:{}", patientId.toString());
+        };
+    }
 }
