f0d79dc1ab4dfc0353b37e62d3d25a65b2094eed
Pato <75866080+warrior226@users.noreply.github.com>
2025-10-09 02:05:36 +0000

    Implement & Demo of Async communication or event streaming using Kafka

--- consultation-service/src/main/java/com/wendyam_rayaisse/consultationservice/functions/ConsultationFunctions.java
+++ consultation-service/src/main/java/com/wendyam_rayaisse/consultationservice/functions/ConsultationFunctions.java
@@ -3,6 +3,7 @@ package com.wendyam_rayaisse.consultationservice.functions;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.util.function.Consumer;
@@ -12,6 +13,7 @@ import java.util.function.Consumer;
 public class ConsultationFunctions {
     private static final Logger log = LoggerFactory.getLogger(ConsultationFunctions.class);
 
+    @Bean
     public Consumer<Long> updateCommunication(){
 
         return patientId->{
